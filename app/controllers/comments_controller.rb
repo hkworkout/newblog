169d25ba844a49941f3eca98d03b4c6a20e443e9
Hari Bellam <hkworkout@gmail.com>
2017-05-14 17:28:14 -0500

    first commit

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,26 @@
+class CommentsController < ApplicationController
+  def create
+        @article = Article.find(params[:article_id])
+        @comment = @article.comments.create(comment_params)
+ 
+      if @comment.save
+        redirect_to article_path(@article)
+      else
+        render 'new'
+      end
+  end
+  def destroy
+        @article = Article.find(params[:article_id])
+        @comment = @article.comments.find(params[:id])
+
+        @comment.destroy
+
+        redirect_to article_path(@article)
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:commenter, :body)
+  end
+
+end
